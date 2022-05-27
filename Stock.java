dd298ab0badbd3adb2e957a5ecc223bbc51de9e0
narsing17 <narsingp14@gmail.com>
2022-05-28 00:20:54 +0530

    class to store stock details

---
+++ Stock.java
@@ -0,0 +1,31 @@
+package stock.com;
+
+public class Stock {
+    private String stockName;
+    private int price;
+    private int numberOfShare;
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getNumberOfShare() {
+        return numberOfShare;
+    }
+
+    public void setNumberOfShare(int numberOfShare) {
+        this.numberOfShare = numberOfShare;
+    }
+}
