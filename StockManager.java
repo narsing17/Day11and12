5a6ec08f28d77eb3f5e92aab31e7eb976bd52f3a
narsing17 <narsingp14@gmail.com>
2022-05-28 00:22:02 +0530

    created list of stock objects

---
+++ StockManager.java
@@ -0,0 +1,40 @@
+package stock.com;
+import java.util.Scanner;
+public class StockManager {
+    public static void main(String[] args) {
+
+        Scanner scannerObject = new Scanner(System.in);
+
+        Stock[] stockArray = new Stock[3];
+        int arrayIndex = 0;
+        Stock stockObject = new Stock();
+        StockOperations operationObject = new StockOperations();
+
+        do {
+            System.out.println("1 : Add Stock\n2 : Value Of each inventory\n3 : Total Value Of Stock");
+            int userChoice = scannerObject.nextInt();
+            switch(userChoice) {
+                case 1:
+                    stockObject = operationObject.addStock();
+                    stockArray[arrayIndex] = stockObject;
+                    arrayIndex++;
+                    break;
+
+                case 2:
+
+                    operationObject.calculateEachStockValue(stockArray);
+                    break;
+
+                case 3:
+                    operationObject.calculateTotalValue(stockArray);
+                    break;
+            }
+
+            System.out.println("If you want to perform more Operations Enter true");
+
+        }while(scannerObject.nextBoolean());
+
+        scannerObject.close();
+    }
+
+}
